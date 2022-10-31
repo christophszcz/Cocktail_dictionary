80714902c7eadc3a28274762594bfcc3570be2a9
Gowrishankar04 <shankargowri906@gmail.com>
2022-11-01 02:48:51 +0530

    Added about us page

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About = () => (
+  <div className={styles.About}>
+    <h1>About Us</h1>
+    <p>
+      This project is going to showcase various types of cocktails and how to make them. It will act as a cocktail dictionary.
+    </p>
+  </div>
+);
+
+export default About;
