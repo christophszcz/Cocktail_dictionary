00f850dce66ef84d471fe7f6d953213abb06707b
Lehebax <117118761+Lehebax@users.noreply.github.com>
2022-11-01 02:09:07 +0530

    Add files via upload

---
+++ src/DUMMY_DATA.js
@@ -0,0 +1,43 @@
+export const dummy_data = [
+	{
+	  id: 1,
+	  name: "747",
+	  category: "Shot",
+	  info: "Alcoholic",
+	  glass: "Shot Glass",
+	  instructons:
+		"Pour Kaluha, Then Baileys, Then Frangelico Not Chilled And Not Layered -- SERVE!!!",
+	  ingredients: ["Kahlua", "Baileys Irish Cream", "Frangelico"],
+	  imageName: "747.jpg",
+	},
+	{
+	  id: 2,
+	  name: "GG",
+	  category: "Shot",
+	  info: "Alcoholic",
+	  glass: "Shot Glass",
+	  instructons: "Layered In A Shot Glass.",
+	  ingredients: ["Amaretto", "Baileys Irish Cream", "Cognac"],
+	  imageName: "gg.jpg",
+	},
+	{
+	  id: 3,
+	  name: "A1",
+	  category: "Shot",
+	  info: "Alcoholic",
+	  glass: "Shot Glass",
+	  instructons: "Layered In A Shot Glass.",
+	  ingredients: ["Amaretto", "Baileys Irish Cream", "Cognac"],
+	  imageName: "a1.jpg",
+	},
+	{
+	  id: 4,
+	  name: "ABC",
+	  category: "Shot",
+	  info: "Alcoholic",
+	  glass: "Shot Glass",
+	  instructons: "Layered In A Shot Glass.",
+	  ingredients: ["Amaretto", "Baileys Irish Cream", "Cognac"],
+	  imageName: "abc.jpg",
+	},
+  ];
